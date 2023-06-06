0d426561db5af6d89f0e405fcb911fff65bd7eb8
mg137137 <gmihir11@gmail.com>
2023-06-06 19:59:56 +0530

    hello

--- lib/Delar List Button/Delar Detail.dart
+++ lib/Delar List Button/Delar Detail.dart
@@ -11,7 +11,8 @@ class Delar_Detail extends StatefulWidget {
   final String token;
   final String id;
 
-  const Delar_Detail({super.key, required this.token, required this.id});
+  const Delar_Detail({Key? key, required this.token, required this.id})
+      : super(key: key);
 
   @override
   State<Delar_Detail> createState() => _Delar_DetailState();
@@ -19,6 +20,8 @@ class Delar_Detail extends StatefulWidget {
 
 class _Delar_DetailState extends State<Delar_Detail> {
   Map<String, dynamic> data = {};
+  Map<String, dynamic> testing = {};
+
   Future<void> getData() async {
     final response = await http.get(
       Uri.parse(
@@ -29,53 +32,23 @@ class _Delar_DetailState extends State<Delar_Detail> {
     if (response.statusCode == 200) {
       setState(() {
         data = jsonDecode(response.body);
+        testing = data['DealerCounterdetails'];
+        print('The response data is ' + "$testing");
       });
     } else {
-      print('Request failed with status: ${response.statusCode}.');
+      print('Request failed with status: ${response.statusCode}');
     }
   }
 
-  // late Map<String, dynamic> data;
-  // bool isLoggedIn = false;
-  // String _response = '';
-  // SharedPreferences? _prefs;
-  // List<dynamic> _booklistdetail = [];
-  // int length = 1;
-  //
-  // List<modelclass> detailist = [];
-
   @override
   void initState() {
-    // fetchData();
     super.initState();
     getData();
   }
-  // Future<List<modelclass>> fetchData() async {
-  //   // _prefs = await SharedPreferences.getInstance();
-  //   final response = await http.get(
-  //     Uri.parse('http://192.168.0.114:8003/mobileApprouter/getBookList'),
-  //     headers: {'Authorization': 'Bearer ${widget.token}'},
-  //   );
-  //   var data = jsonDecode(response.body.toString());
-  //   if (response.statusCode == 200) {
-  //     for (Map i in data) {
-  //       // print(i['Vehicle Details']);
-  //       Map<String, dynamic> data = jsonDecode(response.body);
-  //       print(data['Vehicle Details']);
-  //       // print(data['Owner Details']['Address']);
-  //       // print(data['Insurance Details']['Policy Number']);
-  //       // print(data['Status']['Status']);
-  //       // print(data['Customer Details']['workType']);
-  //     }
-  //     return detailist;
-  //   } else {
-  //     return detailist;
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
-    final Decoration Bluebox_Decoration = BoxDecoration(
+    final Decoration blueBoxDecoration = BoxDecoration(
       color: Colors.blue,
       boxShadow: [
         BoxShadow(
@@ -87,159 +60,161 @@ class _Delar_DetailState extends State<Delar_Detail> {
       borderRadius: BorderRadius.circular(10),
     );
 
-    var arraybox_title = [
-      'vehicle detail',
-      'vehicle buyer detail',
-      'vehicle insurance detail',
-      'vehicle insurance detail',
-    ];
-    var arrayname_title = [
-      'Name',
-      'Name',
-      'Name',
-      'Name',
-    ];
-    var arrayname_data = ['mihie', 'mihir', 'gandhi', 'hello'];
-
-    var arrayaddresh_title = ['addresh', 'addresh', 'addresh', 'addresh'];
-    var arrayaddresh_data = ['rajkot', 'junagadh', 'jamnagar', 'ahemdabad'];
-
     return Scaffold(
       body: SingleChildScrollView(
-          child: SafeArea(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                // decoration: BoxDecoration(
-                //     gradient: LinearGradient(
-                //         begin: Alignment.topLeft,
-                //         end: Alignment.bottomRight,
-                //         colors: [
-                //       Color(0xff1a73e8),
-                //       Color(0xff1a73e8),
-                //     ])),
-                height: 60,
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IconButton(
+        child: SafeArea(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.zero,
+                  padding: EdgeInsets.zero,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
                         onPressed: () {
                           Navigator.pop(context);
-
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //       builder: (context) => Dashboard(
-                          //             token: '$stoken',
-                          //           )),
-                          // );
                         },
                         icon: const Icon(
                           Icons.arrow_back_ios_new_outlined,
                           color: Colors.white,
-                        )),
-                    Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0)),
-                    Text(
-                      'Delar Detail',
-                      style: TextStyle(
+                        ),
+                      ),
+                      const Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0)),
+                      Text(
+                        'Dealer Detail',
+                        style: const TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-              new Expanded(
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(0, 7, 0, 0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(40),
-                        topLeft: Radius.circular(40)),
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      var key = data.keys.elementAt(index);
-                      var value = data[key];
-                      print(value.toString());
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 7),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(40),
+                        topLeft: Radius.circular(40),
+                      ),
+                    ),
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        if (index < data.length) {
+                          var key = data.keys.elementAt(index);
+                          var value = data[key];
 
-                      return Stack(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  offset: Offset(4, 5),
-                                  blurRadius: 10,
-                                  color: Colors.black.withOpacity(0.3),
-                                ),
-                              ],
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            margin: EdgeInsets.fromLTRB(20, 40, 20, 10),
-                            width: MediaQuery.of(context).size.width,
-                            child: Container(
-                              padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                              margin: EdgeInsets.fromLTRB(20, 45, 0, 0),
-                              child: SingleChildScrollView(
+                          return Stack(
+                            children: [
+                              Container(
                                 child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    for (var entry in value.entries)
-                                      Text(
-                                        entry.key +
-                                            ' :- ' +
-                                            entry.value.toString(),
-                                        style: TextStyle(fontSize: 15),
-                                        textAlign: TextAlign.left,
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        boxShadow: [
+                                          BoxShadow(
+                                            offset: Offset(4, 5),
+                                            blurRadius: 10,
+                                            color:
+                                                Colors.black.withOpacity(0.3),
+                                          ),
+                                        ],
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
                                       ),
+                                      margin: const EdgeInsets.fromLTRB(
+                                          20, 40, 20, 10),
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Container(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 0, 0, 5),
+                                        margin: const EdgeInsets.fromLTRB(
+                                            20, 45, 0, 0),
+                                        child: SingleChildScrollView(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              for (var entry in value.entries)
+                                                Text(
+                                                  '${entry.key} :- ${entry.value}',
+                                                  style: const TextStyle(
+                                                      fontSize: 15),
+                                                  textAlign: TextAlign.left,
+                                                ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
                                   ],
                                 ),
                               ),
-                            ),
-                          ),
-                          Positioned(
-                            top: -10,
-                            left: 30,
-                            right: 30,
-                            child: Container(
-                              alignment: Alignment.center,
-                              height: 50,
-                              width: 350,
-                              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                              decoration: Bluebox_Decoration,
-                              child: Text(
-                                key,
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.black),
+                              Positioned(
+                                top: -10,
+                                left: 30,
+                                right: 30,
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  height: 50,
+                                  width: 350,
+                                  margin:
+                                      const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                                  decoration: blueBoxDecoration,
+                                  child: Text(
+                                    key,
+                                    style: const TextStyle(
+                                        fontSize: 15, color: Colors.black),
+                                  ),
+                                ),
                               ),
+                            ],
+                          );
+                        } else {
+                          // Display the static buttons
+                          int buttonIndex = index - data.length;
+
+                          var key = testing.keys.elementAt(buttonIndex);
+                          var value =
+                              testing[testing.keys.elementAt(buttonIndex)];
+
+                          return Container(
+                            margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Handle button press based on buttonIndex
+                              },
+                              child: Text('$key' + '$value'),
                             ),
-                          )
-                        ],
-                      );
-                    },
-                    itemCount: data.length,
+                          );
+                        }
+                      },
+                      itemCount: data.length +
+                          testing.length, // Add 6 for the static buttons
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
